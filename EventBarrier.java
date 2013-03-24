6b715d1a25b83a457b682278bed0bfc9d5b011e1
Tianyu Shi <ts133@duke.edu>
2013-03-24 18:12:22 -0400

    debugging eventbarriertest and eventbarrier

--- EventBarrier.java
+++ EventBarrier.java
@@ -1,14 +1,17 @@
 public class EventBarrier extends AbstractEventBarrier {
 
 	boolean gatedown = false;
+	int totaltasks = 0;
 	int waitercount = 0;
 	int crossingcount = 0;
 
 	public EventBarrier(int numWorkers) {
 		super(numWorkers);
+		totaltasks = numWorkers;
 	}
 
 	public synchronized void arrive() {
+		System.out.println("arrive (start)");
 		waitercount++;
 		while(gatedown) {
 			try {
@@ -19,22 +22,26 @@ public class EventBarrier extends AbstractEventBarrier {
 		}
 		waitercount--;
 		crossingcount++;
+		System.out.println("arrive (end)");
 	}
 
 	public synchronized void raise() {
+		System.out.println("raise (start)");
 		gatedown = false;
-			this.notifyAll();
+		this.notifyAll();
 		while(crossingcount>0) {
-				try {
-					this.wait();
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
+			try {
+				this.wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 		gatedown = true;
+		System.out.println("raise (start)");
 	}
 
 	public synchronized void complete() {
+		System.out.println("complete (start)");
 		crossingcount--;
 		while(crossingcount>0) {
 			try {
@@ -44,6 +51,7 @@ public class EventBarrier extends AbstractEventBarrier {
 			}
 		}
 		this.notifyAll();
+		System.out.println("complete (end)");
 	}
 
 	public synchronized int waiters() {
--- EventBarrierTest.java
+++ EventBarrierTest.java
@@ -3,13 +3,14 @@ public class EventBarrierTest {
 	static int numWorkers = 10;
 
 	public static void main(String[] args) {
+
 		EventBarrier e = new EventBarrier(numWorkers);
 
-		e.raise();
-
 		for(int i=0; i< numWorkers; i++) {
+			System.out.println("---" + i + " ---");
 			PrintThread p = new PrintThread(i, e);
 			p.start();
+			e.raise();
 		}
 	}
 }
