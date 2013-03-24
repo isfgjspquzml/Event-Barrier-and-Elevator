e9eb5d5e9846316dc932233908c6c56483290898
Tianyu Shi <ts133@duke.edu>
2013-03-24 03:43:11 -0400

    start of event barriertest

--- EventBarrierTest.java
+++ EventBarrierTest.java
@@ -1,9 +1,15 @@
-public class EventBarrierTest implements Runnable {
+public class EventBarrierTest {
 
-	int numWorkers = 10;
-	
-	public void run() {
+	static int numWorkers = 10;
+
+	public static void main(String[] args) {
 		EventBarrier e = new EventBarrier(numWorkers);
-		
+
+		e.raise();
+
+		for(int i=0; i< numWorkers; i++) {
+			PrintThread p = new PrintThread(i, e);
+			p.start();
+		}
 	}
 }
--- PrintThread.java
+++ PrintThread.java
@@ -1,11 +1,16 @@
 public class PrintThread extends Thread{
 	
 	long number;
-	PrintThread(long num) {
+	EventBarrier e;
+	
+	PrintThread(long num, EventBarrier e) {
 		this.number = num;
+		this.e = e;
 	}
 	
 	public void run() {
+		e.arrive();
 		System.out.println(number);
+		e.complete();
 	}
 }
