4397477715835cb5b412c6a9648b43567457dc40
Tianyu Shi <ts133@duke.edu>
2013-03-25 11:09:39 -0400

    t

--- Building.java
+++ Building.java
@@ -1,9 +1,30 @@
+import java.util.ArrayList;
+
 public class Building extends AbstractBuilding {
 
-	public Building(int numFloors, int numElevators) {
+	int numFloors;
+	int numElevators;
+	int maxOccupancyThreshold;
+	
+	ArrayList<Elevator> ElevatorList = new ArrayList<Elevator>();
+	ArrayList<Thread> TaskList = new ArrayList<Thread>();
+	
+	public Building(int numFloors, int numElevators, int maxOccupancyThreshold) {
 		super(numFloors, numElevators);
+		this.numFloors = numFloors;
+		this.numElevators = numElevators;
+		this.maxOccupancyThreshold = maxOccupancyThreshold;
+		
+		for(int i=0; i<numElevators; i++) {
+			ElevatorList.add(new Elevator(numFloors, i, maxOccupancyThreshold));
+			TaskList.add(new Thread(ElevatorList.get(i)));
+		}
+		
+		for(Thread task:TaskList) {
+			task.start();
+		}
 	}
-
+	
 	public void CallUp(int fromFloor) {
 		// TODO Auto-generated method stub
 		
